ac342918482021fe386f27cd3ed6864a5e50acaf
jigarzotech <jigarzotech@gmail.com>
2022-12-30 12:18:46 +0530

    Add NextJs tutorial

--- src/components/home/home-page.js
+++ src/components/home/home-page.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import styles from '../../../styles/Home.module.css'
 
 export const HomePage = ({ data }) => (
     <div className="home_body">
